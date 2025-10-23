f78a9b48ca851693fe95012f5cb1fb8cfab11cba
corey566 <112794552+corey566@users.noreply.github.com>
2025-10-23 11:25:54 +0000

    Restored to 'ef504f8c4c30833561e345c941866a986ce4ec9e'
    Replit-Restored-To: ef504f8c4c30833561e345c941866a986ce4ec9e

--- changed_files_backup_20251023_091116/Modules/AiAssistance/Config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'name' => 'AiAssistance',
-    'module_version' => '2.0',
-    'pid' => 17,
-];
--- changed_files_backup_20251023_091116/Modules/ProductCatalogue/Config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'name' => 'ProductCatalogue',
-    'module_version' => '2.1',
-    'pid' => 8,
-];
